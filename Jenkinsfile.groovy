7ab425e91f6fcd57f6ee6b17357e15d0ed6a3d88
CZ08 <selim.borgi@esprit.tn>
2025-11-27 18:39:52 +0100

    Ajout Jenkinsfile
    Commit

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -24,7 +24,7 @@ pipeline {
         stage('Code Checkout') {
             steps {
                 git branch: 'main', 
-                    url: 'https://github.com/CZ08/Devops'
+                    url: 'https://github.com/CZ08/Devops.git'
             }
         }
 
